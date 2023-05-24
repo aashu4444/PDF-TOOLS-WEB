1cc37f67586e0bf8c8e96c22f470925a29ab650b
Ashutosh <prajapatiaashu4444@gmail.com>
2023-05-24 17:51:17 +0530

    Deletd unused files and changed manifest file

--- pdf-tools/src/components/deleteFileFromServerBtn.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Globals from './globals';
-
-export default function deleteFileFromServerBtn(props) {
-    return (
-        <button onClick={e => Globals.removeFileFromServer(props.result.responseText)} className="btn-large theme-bg waves-effect waves-light">Delete file from server</button>
-    )
-}
