88388f8f648fed041b8acc265c49e7f9d01346b4
Ashutosh <prajapatiaashu4444@gmail.com>
2023-05-27 11:40:35 +0530

    Fixed envrionment variable name

--- pdftools-frontend/src/components/globals.js
+++ pdftools-frontend/src/components/globals.js
@@ -3,7 +3,7 @@ import Request from './request';
 
 const globals = {
     url:function(url){
-        return `http://${process.env.HOST}${url}`
+        return `http://${process.env.REACT_APP_HOST}${url}`
     },
     addFile:function(pdfName, pdfUrl){
         // The data to be added in localStorage.
